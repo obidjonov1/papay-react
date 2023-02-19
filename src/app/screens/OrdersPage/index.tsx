22a3354e0b734c15b2e9c3df255eb6955dd6c63d
obidjonov1 <obidjonov@naver.com>
2023-02-19 22:35:29 +0900

    feat: develop routing based on papay project

---
+++ src/app/screens/OrdersPage/index.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/system";
+import React from "react";
+
+export function OrdersPage() {
+  return <Container>OrdersPage</Container>;
+}
---
+++ src/app/screens/RestaurantPage/index.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/system";
+import React from "react";
+
+export function RestaurantPage() {
+  return <Container>RestaurantPage</Container>;
+}
