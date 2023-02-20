64e934dd7b6368ddc272032ba202a1992a3b9f25
obidjonov1 <obidjonov@naver.com>
2023-02-20 22:07:47 +0900

    feat: develop header navbarHome component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "../css/App.css";
+import "../css/navbar.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { RestaurantPage } from "./screens/RestaurantPage";
 import { CommunityPage } from "./screens/CommunityPage";
@@ -9,12 +10,25 @@ import { MemberPage } from "./screens/MemberPage";
 import { HelpPage } from "./screens/HelpPage";
 import { LoginPage } from "./screens/LoginPage";
 import { HomePage } from "./screens/HomePage";
+import { NavbarHome } from "./components/header";
+import { NavbarRestaurant } from "./components/header/restaurant";
+import { NavbarOthers } from "./components/header/others";
 
 function App() {
+  const [path, setPath] = useState();
+  const main_path = window.location.pathname;
+
   return (
     <Router>
-      <div>
-        <nav>
+      {main_path == "/" ? (
+        <NavbarHome setPath={setPath} />
+      ) : main_path.includes("/restaurant") ? (
+        <NavbarRestaurant setPath={setPath} />
+      ) : (
+        <NavbarOthers setPath={setPath} />
+      )}
+
+      {/* <nav>
           <ul>
             <li>
               <Link to="/restaurant">RestaurantPage</Link>
@@ -38,32 +52,31 @@ function App() {
               <Link to="/">HomePage</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
 
-        <Switch>
-          <Route path="/restaurant">
-            <RestaurantPage />
-          </Route>
-          <Route path="/community">
-            <CommunityPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <MemberPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/restaurant">
+          <RestaurantPage />
+        </Route>
+        <Route path="/community">
+          <CommunityPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <MemberPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
